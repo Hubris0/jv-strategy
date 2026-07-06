615985c50a260707c2809b6e0af36ef68cd4ef94
user <pawel.m.janik@gmail.com>
2026-07-06 13:23:20 +0200

    Task complete. Created Strategy pattern for DiscountService.  Implemented BirthdayDiscountService, BlackFridayDiscountService, NewYearDiscountService and DefaultDiscountService.

---
+++ src/main/java/core/basesyntax/DiscountStrategy.java
@@ -0,0 +1,15 @@
+package core.basesyntax;
+
+import core.basesyntax.strategy.DiscountService;
+import core.basesyntax.strategy.impl.*;
+
+public class DiscountStrategy {
+    public DiscountService getDiscountServiceBySpecialEvent(String specialEvent) {
+        return switch (specialEvent) {
+            case "Birthday" -> new BirthdayDiscountService();
+            case "Black Friday" -> new BlackFridayDiscountService();
+            case "New Year" -> new NewYearDiscountService();
+            default -> new DefaultDiscountService();
+        };
+    }
+}
